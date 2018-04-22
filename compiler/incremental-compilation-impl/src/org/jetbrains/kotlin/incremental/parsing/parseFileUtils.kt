9b09d8008493ac87bd3e0ecb374d6504f644bc10
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2018-04-23 00:34:23 +0300

    ~ lexer only fq names

--- compiler/incremental-compilation-impl/src/org/jetbrains/kotlin/incremental/parsing/parseFileUtils.kt
+++ compiler/incremental-compilation-impl/src/org/jetbrains/kotlin/incremental/parsing/parseFileUtils.kt
@@ -5,67 +5,101 @@
 
 package org.jetbrains.kotlin.incremental.parsing
 
-import com.intellij.openapi.Disposable
-import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.vfs.StandardFileSystems
-import com.intellij.openapi.vfs.VirtualFileManager
-import com.intellij.openapi.vfs.local.CoreLocalFileSystem
-import com.intellij.psi.PsiManager
-import com.intellij.psi.SingleRootFileViewProvider
-import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
-import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
-import org.jetbrains.kotlin.config.CompilerConfiguration
-import org.jetbrains.kotlin.psi.KtClassOrObject
-import org.jetbrains.kotlin.psi.KtDeclaration
-import org.jetbrains.kotlin.psi.KtFile
+import com.intellij.psi.tree.IElementType
+import org.jetbrains.kotlin.lexer.KotlinLexer
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.name.Name
 
 import java.io.File
 import java.util.*
+import kotlin.collections.HashSet
 
 fun classesFqNames(files: Set<File>): Set<String> {
     val existingKotlinFiles = files.filter { it.name.endsWith(".kt", ignoreCase = true) && it.isFile }
-    if (existingKotlinFiles.isEmpty()) return emptySet()
-
-    val disposable = Disposer.newDisposable()
-
-    return try {
-        classesFqNames(existingKotlinFiles, disposable)
-    } finally {
-        Disposer.dispose(disposable)
-    }
-}
-
-private fun classesFqNames(kotlinFiles: Collection<File>, disposable: Disposable): Set<String> {
-    val config = CompilerConfiguration()
-    val configFiles = EnvironmentConfigFiles.JVM_CONFIG_FILES
-    val environment = KotlinCoreEnvironment.createForProduction(disposable, config, configFiles)
-    val psiManager = PsiManager.getInstance(environment.project)
-    val fileManager = VirtualFileManager.getInstance()
-    val localFS = fileManager.getFileSystem(StandardFileSystems.FILE_PROTOCOL) as CoreLocalFileSystem
 
     val result = HashSet<String>()
-
-    for (file in kotlinFiles) {
-        val virtualFile = localFS.findFileByIoFile(file)!!
-
-        for (psiFile in SingleRootFileViewProvider(psiManager, virtualFile).allFiles) {
-            if (psiFile !is KtFile) continue
-
-            val classes = ArrayDeque<KtClassOrObject>()
-            psiFile.declarations.filterClassesTo(classes)
-            while (classes.isNotEmpty()) {
-                val klass = classes.pollFirst()
-                klass.fqName?.let {
-                    result.add(it.asString())
-                }
-                klass.declarations.filterClassesTo(classes)
-            }
-        }
+    for (file in existingKotlinFiles) {
+        KotlinSourceClassIdsReader(file).readClassFqNamesTo(result)
     }
 
     return result
 }
 
-private fun Collection<KtDeclaration>.filterClassesTo(classes: Deque<KtClassOrObject>) {
-    filterIsInstanceTo<KtClassOrObject, Deque<KtClassOrObject>>(classes)
+private class KotlinSourceClassIdsReader(file: File) {
+    private val lexer = KotlinLexer().apply {
+        start(file.readText())
+    }
+
+    private var braceBalance = 0
+    private var currentScope: FqName = FqName.ROOT
+    private var lastSeenDeclaration: FqName = FqName.ROOT
+    private var scopes = Stack<Scope>()
+
+    private class Scope(val currentScope: FqName, val lastSeenDeclaration: FqName)
+
+    private fun at(type: IElementType): Boolean = lexer.tokenType == type
+
+    private fun end(): Boolean = lexer.tokenType == null
+
+    private fun advance() {
+        when {
+            at(KtTokens.LBRACE) -> {
+                scopes.add(Scope(currentScope, lastSeenDeclaration))
+                currentScope = lastSeenDeclaration
+                braceBalance++
+            }
+            at(KtTokens.RBRACE) -> {
+                val prev = scopes.pop()
+                currentScope = prev.currentScope
+                lastSeenDeclaration = prev.lastSeenDeclaration
+                braceBalance--
+            }
+        }
+        lexer.advance()
+    }
+
+    private fun tokenText(): String = lexer.tokenText
+
+    private fun atClass(): Boolean =
+        lexer.tokenType in CLASS_KEYWORDS
+
+    fun readClassFqNamesTo(result: HashSet<String>) {
+        while (!end() && !at(KtTokens.PACKAGE_KEYWORD) && !atClass()) {
+            advance()
+        }
+        if (at(KtTokens.PACKAGE_KEYWORD)) {
+            advance()
+
+            while (!end() && !at(KtTokens.IDENTIFIER) && !atClass()) {
+                advance()
+            }
+
+            val packageName = StringBuilder()
+            while (!end() && (at(KtTokens.IDENTIFIER) || at(KtTokens.DOT))) {
+                packageName.append(tokenText())
+                advance()
+            }
+            currentScope = FqName(packageName.toString())
+            lastSeenDeclaration = currentScope
+        }
+
+        while (true) {
+            while (!end() && !atClass()) {
+                advance()
+            }
+            if (end()) break
+            while (!end() && !at(KtTokens.IDENTIFIER)) {
+                advance()
+            }
+            if (end()) break
+            val fqName = currentScope.child(Name.identifier(tokenText()))
+            result.add(fqName.asString())
+            lastSeenDeclaration = fqName
+        }
+    }
+
+    companion object {
+        private val CLASS_KEYWORDS = setOf(KtTokens.CLASS_KEYWORD, KtTokens.INTERFACE_KEYWORD, KtTokens.OBJECT_KEYWORD)
+    }
 }
